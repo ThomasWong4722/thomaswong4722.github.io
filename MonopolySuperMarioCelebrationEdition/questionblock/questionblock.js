1173b07854b9ce0462892288750ea7c03a661702
ThomasWong4722 <whoulong722@gmail.com>
2025-05-18 12:23:55 +0800

    Summary

---
+++ MonopolySuperMarioCelebrationEdition/questionblock/questionblock.js
@@ -0,0 +1,16 @@
+const sounds = ["sound/1.mp3", "sound/2.mp3", "sound/3.mp3", "sound/4.mp3", "sound/power-up.mp3", "sound/death.mp3", "sound/bowser-laugh.mp3"]; 
+        
+        let audioPlaying = false; // Flag to track whether a sound is playing
+
+        document.getElementById("image").addEventListener("click", () => {
+            if (audioPlaying) return; // Prevent function if sound is playing
+
+            audioPlaying = true; // Set flag to true
+            const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+            const audio = new Audio(randomSound);
+
+            audio.play();
+            audio.addEventListener("ended", () => {
+                audioPlaying = false; // Reset flag when sound ends
+            });
+        });
